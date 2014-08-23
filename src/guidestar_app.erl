3f4875116d83d3febea5235e20b27507c33ea907
Aaron Spiegel <Aaron@kapowmedia.net>
2014-08-23 12:36:14 -0500

    Initial commit

---
+++ src/guidestar_app.erl
@@ -0,0 +1,16 @@
+-module(guidestar_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    guidestar_sup:start_link().
+
+stop(_State) ->
+    ok.
