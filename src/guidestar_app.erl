8147c2c3ee4eef4a25c465888c58477fad335841
Aaron Spiegel <Aaron@kapowmedia.net>
2014-08-28 07:35:29 -0500

    Create base guidestar module

--- src/guidestar_app.erl
+++ src/guidestar_app.erl
@@ -2,15 +2,10 @@
 
 -behaviour(application).
 
-%% Application callbacks
 -export([start/2, stop/1]).
 
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
-
-start(_StartType, _StartArgs) ->
-    guidestar_sup:start_link().
+start(_Type, _StartArgs) ->
+  guidestar_sup:start_link().
 
 stop(_State) ->
-    ok.
+  ok.
--- src/guidestar_sup.erl
+++ src/guidestar_sup.erl
@@ -8,20 +8,11 @@
 %% Supervisor callbacks
 -export([init/1]).
 
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
-
-%% ===================================================================
-%% API functions
-%% ===================================================================
+-define(SERVER, ?MODULE).
 
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
-
-%% ===================================================================
-%% Supervisor callbacks
-%% ===================================================================
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
-
+    RestartStrategy = {one_for_one, 0, 1},
+    {ok, {RestartStrategy, []}}.
